e4b9453e00335cb218b7e0df5dbe827405e1276c
Vincent Maret <vincent.maret@verallia.com>
2019-09-27 14:39:18 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
+
+const SRC_PATH = path.resolve(__dirname, 'src');
+const STATIC_PATH = path.resolve(__dirname, 'public');
+const BUILD_PATH = path.resolve(__dirname, 'dist');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: path.resolve(SRC_PATH, 'index.js'),
+  output: {
+    path: BUILD_PATH,
+    filename: "./script.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            }
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.min.css'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Home',
+      template: path.resolve(STATIC_PATH, 'index.html')
+    }),
+    new CopyWebpackPlugin([{
+      from: STATIC_PATH,
+      to: BUILD_PATH,
+      toType: 'dir'
+    }])
+  ],
+  devServer: {
+    contentBase: BUILD_PATH,
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+    liveReload: false,
+    open: false,
+    port: 8080,
+    stats: {
+      children: false, // Hide children information
+      maxModules: 0 // Set the maximum number of modules to be shown
+    }
+  }
+}
